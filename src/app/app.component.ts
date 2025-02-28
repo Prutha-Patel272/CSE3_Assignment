cf748b47c762794794e5e33b5729055dc3f36eb6
PruthaPatel <patelprutha499@gmail.com>
2025-02-28 19:24:49 +0530

    Angular Assignment-2

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { SearchComponent } from './search/search.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet,RouterLink,RouterLinkActive],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Assignment';
+}
---
+++ src/app/search/search.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent {
+  search(query: string) {
+    console.log("Search Query:", query);
+  }
+}
